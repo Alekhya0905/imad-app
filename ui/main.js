c5e510a4a9d8e988a56caec694785a27a9ecc9d8
Alekhya Tethali <alekhya.0905@gmail.com>
2017-08-07 13:26:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
+
 //changing the stmts
 var element = document.getElementById('main-text');
 element.innerHTML  = 'New Value';
+
+//move image
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginleft = '100px'
+}
