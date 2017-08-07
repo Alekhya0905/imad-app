5bf3b62a1650698f3389b67a4fc18df168fbb0cf
Alekhya Tethali <alekhya.0905@gmail.com>
2017-08-07 16:57:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
-
-
 var articles = {
     'article-one' : {
     title: 'Article One | Alekhya',
@@ -58,7 +55,6 @@ var articles = {
             `
     },
 };
-
 function createTemplate(data) {
     var title = data.title;
     var date = data.date;
@@ -95,11 +91,9 @@ function createTemplate(data) {
         `;
     return htmlTemplate;
 }
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
+});\
 var counter = 0;
 app.get('/counter', function(req,res) {
    counter = counter + 1;
@@ -123,6 +117,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function( req, res) {
+   //Get the request from the server
+   var name = req.params.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
