053d8ca0a5085a45dd653fa007f16591b1af2861
Alekhya Tethali <alekhya.0905@gmail.com>
2017-08-07 16:57:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,40 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function() {
-    //make a request to the counter endpoint
+    //create a request object to the counter endpoint
+    var request = new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span =  document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readystate == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200)
+                {
+                    var counter = request.resonseText;
+                    var span =  document.getElementById('count');
+                    span.innerHTML = counter.toString();            
+                }
+        }
+    //not yet done
+    };
+    //make a request
+    request.open('GET','http://coco98.imad.hasura-app.io.counter', true);
+    request.send(null);
+};
+
+//Submit name
+var nameInput =  document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Make a request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
+    for(var i=0 ; i<names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul =  document.getElementById('namelist');
+    ul.innerHTML = list;
 };
